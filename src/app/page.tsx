5282cf127564f03a0ba458bf33aef8b6a91d5d23
foluwa <moronfoluwaakintola@gmail.com>
2024-03-22 00:51:55 +0000

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import EventCard from '../components/Cards/EventCard';
 import TrackCard from '../components/Cards/TrackCard';
 // import Footer from '../components/Footer';
 // import YoutubeSlider from '../components/YoutubeSlider';
-// import { Facebook, XTwitter } from '../../src/components/SocialMedia/index';
 
 import { SocialIcon } from 'react-social-icons'
 
@@ -84,3 +83,36 @@ const Home = () => {
 }
 
 export default Home;
+
+// import * as React from "react"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
